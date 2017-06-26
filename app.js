4afdc3f5efd8600b525151efc3f72145a4cad69d
Leon666666 <j.hu@sofia.edu>
2017-06-26 13:13:19 -0700

    Socket+nodejs

--- app.js
+++ app.js
@@ -1,93 +1,42 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator');
-var mongojs = require("mongojs");
-var db = mongojs('leon',['note']);
-var ObjectId = mongojs.ObjectId;
 var app = express();
-var note = [{'when':'','where':'','what':''}];
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.set('views', path.join('views'));
-app.set('view engine', 'ejs');
-//Body Parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-//global vars
-app.use(function(req, res, next){
-    res.locals.errors = null;
-    next();
+users = [];
+connections = [];
+
+server.listen(3000,function(){
+  console.log('server started on 3000');
 });
-//Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-// Set Static Path
-app.use(express.static('public'));
 
 app.get('/', function(req, res){
-    db.note.find(function(err, docs){
-        //console.log(docs);
-        res.render('index',{
-            title:'Notes:',
-            notes:docs
-        });
-    })
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/note/add',function(req,res){
-    req.checkBody('date','When do you do this?').notEmpty();
-    req.checkBody('where','Where do you do this?').notEmpty();
-    req.checkBody('what','What do you do?').notEmpty();
+io.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+  // disconnect
+  socket.on('disconnect',function(data){
+    users.splice(users.indexOf(socket.username),1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length)
+ });
+ // Send Message
+ socket.on('send message', function(data){
+   io.sockets.emit('new message', {msg: data, user:socket.username});
+ });
+ // new user
+ socket.on('new user', function(data, callback){
+   callback(true);
+   socket.username = data;
+   users.push(socket.username);
+   updateUsernames();
+ });
 
-    var errors = req.validationErrors();
-
-    if(errors){
-        res.render('index',{
-            title:'Notes',
-            notes:note,
-            errors:errors
-          });
-    }else{
-      var newNote = {
-          date:req.body.date,
-          where:req.body.where,
-          what:req.body.what
-      }
-      db.note.insert(newNote,function(err,result){
-          if(err){
-            console.log(err);
-          }
-          res.redirect('/');
-      })
-      console.log('Success');
-    }
-});
-
-app.delete('/note/delete/:id', function(req, res){
-
-      db.note.remove({_id:ObjectId(req.params.id)},function(err,result){
-          if(err){
-              console.log(err);
-          }
-          res.redirect('/');
-          console.log('done');
-      });
-});
-
-app.listen(3000, function(){
-    console.log('Server Started on Port 3000');
+ function updateUsernames(){
+   io.sockets.emit('get users', users);
+ }
 });
